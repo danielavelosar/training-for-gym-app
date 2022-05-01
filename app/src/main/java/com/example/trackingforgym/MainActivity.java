82f232611e2dbc669c12e5d3a25c523f00a7fcf4
jcarranza06 <72419038+jcarranza06@users.noreply.github.com>
2022-05-01 02:59:47 -0500

    avance login-registro

--- app/src/main/java/com/example/trackingforgym/MainActivity.java
+++ app/src/main/java/com/example/trackingforgym/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.trackingforgym;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.StrictMode;
 import android.view.Menu;
+import android.view.View;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.trackingforgym.data.Session;
 import com.example.trackingforgym.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        //DataBase.addUser("ads","asd","asd");
+        new Session();
+        if (!Session.getSession()) {
+            definirSession();
+            System.out.println("paso");
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -47,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void definirSession(){
+        System.out.println("abriendo login");
+        Intent abrirLogin = new Intent(MainActivity.this, login.class);
+        startActivity(abrirLogin);
     }
 
     @Override
