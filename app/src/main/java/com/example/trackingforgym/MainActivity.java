0d2fd51581a7c892be76b38dd70670151084095e
jcarranza06 <72419038+jcarranza06@users.noreply.github.com>
2022-05-01 21:44:34 -0500

    avance

--- app/src/main/java/com/example/trackingforgym/MainActivity.java
+++ app/src/main/java/com/example/trackingforgym/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,9 +13,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trackingforgym.data.Session;
 import com.example.trackingforgym.databinding.ActivityMainBinding;
+import com.example.trackingforgym.ui.Adaptador_rutina_layout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+
+    LinearLayout contenedorSeekBarRutina;
+    RecyclerView recycler;
+    Adaptador_rutina_layout adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,44 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        setElements();
+        //configurarSpinnerHome();
+
+    }
+
+    public void setElements(){
+        contenedorSeekBarRutina=(LinearLayout) findViewById(R.id.contenerdorSeekBarEntrenamiento);
+
+        setRecycler();
+    }
+
+    public void setRecycler(){
+
+        recycler = (RecyclerView) findViewById(R.id.contenedorRutinas);
+        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
+
+        String[] rutinas={
+                "1","2","3","4","1","2","3","4","1","2","3","4"
+        };
+
+        adapter=new Adaptador_rutina_layout(rutinas);
+        recycler.setAdapter(adapter);
+
+    }
+
+    /*public  void  configurarSpinnerHome(){
+        Spinner spinner =(Spinner) findViewById(R.id.spinner);
+        String[] textoSpinner ={"intenso","moderado","suave"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, textoSpinner));
+    }*/
+
+    public void hideShowSeekBar(View view){
+        System.out.println("oprimido");
+        if(contenedorSeekBarRutina.getVisibility()==View.VISIBLE)
+            contenedorSeekBarRutina.setVisibility(View.GONE);
+        else
+            contenedorSeekBarRutina.setVisibility(View.VISIBLE);
     }
 
     public void definirSession(){
