3203899715b99a8534485e4a65d66caa46b9b208
lusanchezmo <98631064+lusanchezmo@users.noreply.github.com>
2022-06-23 18:39:22 -0500

    Update gradle.xml

---
+++ app/src/main/java/com/example/trackingforgym/registro_por_rutina.java
@@ -0,0 +1,14 @@
+package com.example.trackingforgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_por_rutina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_por_rutina);
+    }
+}
