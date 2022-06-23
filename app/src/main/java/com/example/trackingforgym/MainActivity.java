ce39202d9354bdf1cceaccd516761168031cb77f
lusanchezmo <98631064+lusanchezmo@users.noreply.github.com>
2022-06-23 16:57:28 -0500

    version_luis

--- app/src/main/java/com/example/trackingforgym/MainActivity.java
+++ app/src/main/java/com/example/trackingforgym/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-        setElements();
+        //setElements();
         setInfNavButtons();
     }
 
@@ -122,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("de vuelta");
     }
 
-    //tal vez este el error
+    /*
     public void setElements(){
         Button ir_crear_ejercicio=(Button) findViewById(R.id.btnCrearEjercicio);
         ir_crear_ejercicio.setOnClickListener(new View.OnClickListener() {
@@ -133,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    */
 
     public void definirSession(){
         System.out.println("abriendo login");
